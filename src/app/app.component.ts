0cdee4d8b0f13e944f393642e4f305ec992f08e8
omerfrkgrsy <omerfrkgrsy@gmail.com>
2020-09-19 22:25:47 +0300

    Todo App Finish Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Model ,TodoItem} from './model';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  user = 'Ömer Faruk Gürsoy';
-  items=[
-    {description:"Kahvaltı",action:"No"},
-    {description:"Spor",action:"No"},
-    {description:"Fatura",action:"No"},
-    {description:"Sinema",action:"No"}
-  ];
+  model=new Model();
+  isDisplay=false;
+  getName(){
+    return this.model.user;
+  }
+  getItems(){
+    if(this.isDisplay){
+      return this.model.items;
+    }
+    return this.model.items.filter(item=>!item.action);
+  }
+  addItem(value){
+    if(value!=""){
+      this.model.items.push(new TodoItem(value,false));
+    }
+  }
 }
---
+++ src/app/model.ts
@@ -0,0 +1,21 @@
+export class Model{
+    user;
+    items;
+    constructor(){
+        this.user="Ömer Faruk";
+        this.items=[
+            new TodoItem("Spor",true),
+            new TodoItem("Kahvaltı",false),
+            new TodoItem("Kitap Okumak",false),
+            new TodoItem("Sinema",false),
+        ];
+    }
+}
+export class TodoItem{
+    description;
+    action;
+    constructor(description,action){
+        this.description=description;
+        this.action=action;
+    }
+}
