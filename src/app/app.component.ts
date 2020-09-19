34ed3ccdd24e13d7f1b2d8b1a3a78f054db00bdf
omerfrkgrsy <omerfrkgrsy@gmail.com>
2020-09-19 21:58:15 +0300

    Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Ömer Faruk Gürsoy';
+  items=[
+    {description:"Kahvaltı",action:"No"},
+    {description:"Spor",action:"No"},
+    {description:"Fatura",action:"No"},
+    {description:"Sinema",action:"No"}
+  ];
 }
